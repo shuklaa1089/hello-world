00cc156d3772b8b38b5f0c2ba129c72491827e35
Abhishek Shukla <abhishek@Mac-mini.local>
2015-07-03 06:04:39 +0100

    Initial Commit

---
+++ hello world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Abhishek Shukla on 03/07/2015.
+//  Copyright (c) 2015 Abhishek Shukla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
